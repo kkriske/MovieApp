73c37f82a712ac0f8f28e083c1b99a7ce1aa3b84
Kristof <kristoffel.columbus@hotmail.com>
2015-07-14 18:39:32 +0200

    fixed resize

--- src/movieapp/VLCOverlay.java
+++ src/movieapp/VLCOverlay.java
@@ -14,34 +14,41 @@ import simplevlc.SimpleVLCPlayer;
  *
  * @author kristof
  */
-public class VLCOverlay {
+/*public class VLCOverlay {
+
+ private final BorderPane root;
+ private final SimpleVLCPlayer player;
+
+ public VLCOverlay(Stage primstage, BorderPane root) {
+ this.root = root;
+ player = new SimpleVLCPlayer(primstage);
+
+ root.widthProperty().addListener((s, o, n) -> player.setPrefWidth((double) n));
+ root.heightProperty().addListener((s, o, n) -> player.setPrefHeight((double) n));
+
+ }
+
+ public void playMovie(File moviefile) {
+ root.setCenter(player);
+ player.play(moviefile.toString());
+ }
+
+ }*/
+public class VLCOverlay extends SimpleVLCPlayer {
 
     private final BorderPane root;
-    private final SimpleVLCPlayer player;
 
     public VLCOverlay(Stage primstage, BorderPane root) {
+        super(primstage);
         this.root = root;
-        player = new SimpleVLCPlayer(primstage);
 
-        root.widthProperty().addListener((s, o, n) -> player.setPrefWidth((double) n));
-        root.heightProperty().addListener((s, o, n) -> player.setPrefHeight((double) n));
-
-        /*parentProperty().addListener((s, o, n) -> {
-         if (o != null) {
-         maxHeightProperty().unbind();
-         maxWidthProperty().unbind();
-         }
-         sout
-         if (n != null && n instanceof Region) {
-         maxHeightProperty().bind(((Region) n).heightProperty());
-         maxWidthProperty().bind(((Region) n).widthProperty());
-         }
-         });*/
+        prefWidthProperty().bind(root.widthProperty());
+        prefHeightProperty().bind(root.heightProperty());
     }
 
     public void playMovie(File moviefile) {
-        root.setCenter(player);
-        player.play(moviefile.toString());
+        root.setCenter(this);
+        play(moviefile.toString());
     }
 
 }
